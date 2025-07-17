92a28be1e92650e091cf8a71c533484b949bb1ef
Marcus <mvapmarcus@gmail.com>
2025-07-17 11:07:09 -0300

    Update aula2_ex9.py

--- aula2_ex9.py
+++ aula2_ex9.py
@@ -2,7 +2,7 @@ idade = int(input(f'Digite a sua idade:'))
 
 if idade >= 18:
     print('Voto obrigatório')
-elif idade >= 16 and idade <=18:
+elif idade >= 16 and idade <18:
     print('Voto facultativo')
 else:
     print('Não tem idade pra votar')
